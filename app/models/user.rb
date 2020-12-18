24f2939b6cdede329cad75757c1e9f02657e706b
Jocelyn Lee <biheyeblue@gmail.com>
2020-12-18 21:41:26 +0800

    deivse sign-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  extend Devise::Models
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,:confirmable#, :omniauthable
+
+  protected
+  def confirmation_required?
+    false
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+#    omniauth_callbacks: "users/omniauth_callbacks",
+    unlocks: 'users/unlocks'
+}
   root to: 'home#index'
+
+  default_url_options :host => "localhost:3000"
 end
