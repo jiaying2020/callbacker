beb96d154cee98d05377c4c596d85ca27d734c55
Jocelyn Lee <biheyeblue@gmail.com>
2020-12-18 22:46:45 +0800

    reload devise:views

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 }
   root to: 'home#index'
 
-  default_url_options :host => "localhost:3000"
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
+
